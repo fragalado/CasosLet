e2bb839544ad7d96771a541b600dcd70a813ca34
fragalado <frandanigd@gmail.com>
2023-01-30 12:35:40 +0100

    cambios

---
+++ edu.javaScript.introduccion/src/main/webapp/script/FormArray.js
@@ -0,0 +1,7 @@
+/**
+ * RECIBE: El número del formulario que quermos, se empieza por 0
+ * DEVUELVE: Una alerta mostrando el nombre id del formulario
+ */
+ function nombreFormularioPorId(numeroFormulario){
+	 alert(document.forms[numeroFormulario].id);
+ }
---
+++ edu.javaScript.introduccion/src/main/webapp/script/ReferrerFunction.js
@@ -0,0 +1,7 @@
+/**
+ * RECIBE: Nada
+ * DEVUELVE: Una alerta con la URL de la página de la que viene el usuario
+ */
+function paginaDeLaQueVieneElUsuario(){
+	alert(document.referrer);
+}
