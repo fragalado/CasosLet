2b6bf6e660b812d427ce05e429b7a76b77bfa67a
fragalado <frandanigd@gmail.com>
2023-01-30 09:07:03 +0100

    charAt 2

--- edu.javaScript.introduccion/src/main/webapp/script/CharAt.js
+++ edu.javaScript.introduccion/src/main/webapp/script/CharAt.js
@@ -21,3 +21,9 @@ for(let j=0; j < texto.length; j++){
 let primerCaracter = texto.charAt(0);
 let ultimoCaracter = texto.charAt(texto.length - 1);
 alert("Primer caracter: " + primerCaracter + "\nUltimo caracter: " + ultimoCaracter);
+
+/* ESTO IMPRIME POR PANTALLA CADA CARACTER DANDO UN SALTO DE LINEA
+for(let i=0; i < texto.length; i++){
+	document.write(texto.charAt(i) + "<br>");
+}
+*/
